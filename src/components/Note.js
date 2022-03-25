bedb78f9a8a4802952be34d3977ec820d70ed036
rizwan-sayyed <60218766+rizwan-sayyed@users.noreply.github.com>
2022-03-25 20:06:51 +0530

    Add files via upload

---
+++ src/components/Note.js
@@ -0,0 +1,16 @@
+import React from "react";
+const Note=(props)=>{
+    
+    return(
+        <div className="note">
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+            <button onClick={()=>{
+                props.onRemove(props.id);
+            }}>DELETE</button>
+        </div>
+    )
+}
+
+
+export default Note;
