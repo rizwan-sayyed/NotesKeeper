63a723416ab58884e7bae20fce0fb1f7f86f098a
rizwan-sayyed <60218766+rizwan-sayyed@users.noreply.github.com>
2022-03-25 20:06:11 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const y=new Date().getFullYear();
+
+const Footer=()=>{
+    return(
+        <footer>
+            <p>Copyright © {y}</p>
+        </footer>
+    )
+}
+
+export default Footer;
